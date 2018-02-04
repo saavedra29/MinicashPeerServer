438ea4b5a51c343c34fa220f419aed8a00fa821e
Aris Tomaras <arisgold29@gmail.com>
2018-02-04 20:00:48 +0200

    running as daemon, configuration folder creation

--- minicashPS.py
+++ minicashPS.py
@@ -1,10 +1,16 @@
+import os
 import json
 import socketserver
 import re
 import argparse
 import hashlib
+from daemon import DaemonContext
+from daemon.daemon import DaemonOSEnvironmentError
 
 peersMap = {}
+HOMEDIR = ''
+MINICASHDIR = ''
+PEERSFILE = ''
 
 
 class PeerHandler(socketserver.BaseRequestHandler):
@@ -67,7 +73,7 @@ class PeerHandler(socketserver.BaseRequestHandler):
 
         # Write data to disk
         try:
-            with open('peersFile.txt', 'w') as peersFile:
+            with open(PEERSFILE, 'w') as peersFile:
                 try:
                     peersFile.write(json.dumps(peersMap, indent=4))
                 except json.JSONDecodeError as e:
@@ -92,25 +98,66 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--run-locally', action='store_true',
                         help='Be accesible only from localhost')
+    parser.add_argument('--homedir', help='Directory inside which to create the \
+                        .minicashPeerServer configuration directory')
+    args = parser.parse_args()
+    if args.homedir:
+        HOMEDIR = args.homedir
+    else:
+        HOMEDIR = os.getenv('HOME')
+    MINICASHDIR = os.path.join(HOMEDIR, '.minicashPeerServer')
+    
+    # Create the .minicashPeerServer folder and the peers.json file
+    try:
+        os.chdir(HOMEDIR)
+        if not os.path.isdir('.minicashPeerServer'):
+            os.mkdir('.minicashPeerServer')
+        os.chdir(HOMEDIR + '/.minicashPeerServer')
+    except (OSError, PermissionError)as e:
+        print('Error creating or entering .minicashPeerServer directory: {}'.format(str(e)))
+        exit()
+
+    try:
+        if not os.path.isfile('peers.json'):
+            with open('peers.json', 'w') as infile:
+                infile.write('{}')
+    except (OSError, PermissionError)as e:
+        print('Error creating or entering peers.json: {}'.format(str(e)))
+        exit()
+    
+    PEERSFILE = MINICASHDIR + '/peers.json'
     # Load from the memory existing ip peers file
     try:
-        with open('peersFile.txt', 'r') as peersFile:
+        with open(PEERSFILE, 'r') as peersFile:
             try:
                 peersMap = json.load(peersFile)
             except json.JSONDecodeError as e:
-                print('Json Error loading peersFile.txt: {}'.format(e))
+                print('Json Error loading peers.json: {}'.format(str(e)))
                 exit()
     except IOError as e:
-        print('IOError opening peersFile.txt: {}'.format(e))
+        print('IOError opening peers.json: {}'.format(str(e)))
         exit()
 
-    args = parser.parse_args()
     if args.run_locally:
         host = '127.0.0.1'
     else:
         host = ''
-    port = 9999
-    socketserver.TCPServer.allow_reuse_address = True
-    server = socketserver.TCPServer((host, port), PeerHandler)
-    server.serve_forever()
+
+    try:
+        dcontext = DaemonContext(working_directory=MINICASHDIR)
+        try:
+            dcontext.stderr = open(os.path.join(MINICASHDIR, 'miniPeerServer.err'), 'w+')
+        except (OSError, PermissionError) as e:
+            print('Couldn\'t write the miniPeerServer.err file: {}'.format(e))
+            exit()
+        port = 9999
+        with dcontext:
+            socketserver.TCPServer.allow_reuse_address = True
+            server = socketserver.TCPServer((host, port), PeerHandler)
+            server.serve_forever()
+
+    except DaemonOSEnvironmentError as e:
+        print('ERROR: {}'.format(e))
+        stop()
+
 
